1cdee8a475c0ba4a6ecdfbeddfe4c8d44f6374c0
sum <chempionistr@gmail.com>
2021-03-13 17:40:18 +0200

    New Object!

---
+++ src/ObjectsClasses.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class ObjectsClasses {
+    public static void main(String[] args) {
+        new Date();
+        System.out.println(new Date());
+
+        String s = new Date().toString();
+
+        System.out.println(s);
+    }
+}
